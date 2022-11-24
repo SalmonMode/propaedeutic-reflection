e5337db2a1fae1e24aa4f68264abb70e4a984de8
Chris NeJame <chris.t.nejame@gmail.com>
2022-11-24 10:16:13 -0500

    feat: add ability to query for relative score

--- src/ScoreAggregator.spec.ts
+++ src/ScoreAggregator.spec.ts
@@ -1,7 +1,7 @@
 import * as chai from "chai";
 import { NoSelfAssessmentsError } from "./Errors";
 import ScoreAggregator from "./ScoreAggregator";
-import { SelfAssessment } from "./types";
+import { RelativeSelfAssessment, SelfAssessment } from "./types";
 
 var expect = chai.expect;
 
@@ -33,6 +33,49 @@ describe("ScoreAggregator", function () {
     it("should throw NoSelfAssessmentsError", function () {
       const aggregator = new ScoreAggregator();
       expect(() => aggregator.averageScore).to.throw(NoSelfAssessmentsError);
+    });
+  });
+  describe("getRelativeSelfAssessment", function () {
+    const selfAssessments: SelfAssessment[] = [
+      {
+        score: 10,
+      },
+      {
+        score: 0,
+      },
+      {
+        score: 3,
+      },
+      {
+        score: 5,
+      },
+      {
+        score: 7,
+      },
+    ];
+    const testAssessment: SelfAssessment = {
+      score: 4,
+    };
+    const expectedRelSelfAssessment: RelativeSelfAssessment = {
+      ...testAssessment,
+      averageScore: 5,
+    };
+    it("should show relative score", function () {
+      const aggregator = new ScoreAggregator(...selfAssessments);
+      expect(
+        aggregator.getRelativeSelfAssessment(testAssessment)
+      ).to.deep.equal(expectedRelSelfAssessment);
+    });
+  });
+  describe("getRelativeSelfAssessment (no assessments provided)", function () {
+    const testAssessment: SelfAssessment = {
+      score: 4,
+    };
+    it("should throw NoSelfAssessmentsError", function () {
+      const aggregator = new ScoreAggregator();
+      expect(() =>
+        aggregator.getRelativeSelfAssessment(testAssessment)
+      ).to.throw(NoSelfAssessmentsError);
     });
   });
 });
--- src/ScoreAggregator.ts
+++ src/ScoreAggregator.ts
@@ -1,5 +1,5 @@
 import { NoSelfAssessmentsError } from "./Errors";
-import { AverageScore, SelfAssessment } from "./types";
+import { AverageScore, RelativeSelfAssessment, SelfAssessment } from "./types";
 
 export default class ScoreAggregator {
   public selfAssessments: SelfAssessment[];
@@ -24,5 +24,19 @@ export default class ScoreAggregator {
     );
     const averageScore: AverageScore = sum / this.selfAssessments.length;
     return averageScore;
+  }
+  /**
+   * Returns a {@link RelativeSelfAssessment} object showing how the given {@link SelfAssessment} compares against the average.
+   *
+   * @param selfAssessment the {@link SelfAssessment} to compare against the average
+   * @returns a {@link RelativeSelfAssessment} object containing the original {@link SelfAssessment} details along with the average score.
+   */
+  getRelativeSelfAssessment(
+    selfAssessment: SelfAssessment
+  ): RelativeSelfAssessment {
+    return {
+      ...selfAssessment,
+      averageScore: this.averageScore,
+    };
   }
 }
