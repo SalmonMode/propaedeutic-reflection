701f754c9cc0183518641691fb04e6d38cda023a
Chris NeJame <chris.t.nejame@gmail.com>
2022-11-29 15:45:24 -0500

    docs: updated params and decscription

--- src/database/skillArea/createSkillArea.ts
+++ src/database/skillArea/createSkillArea.ts
@@ -2,9 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import { SkillAreaSummary } from "../../types";
 
 /**
- * Create a SkillArea using the description provided.
+ * Create a SkillArea using the title and description provided.
  *
  * @param prisma The client to access the DB with
+ * @param title A title of the SkillArea
  * @param description A description of the SkillArea
  *
  * @returns A summary of the created SkillArea
